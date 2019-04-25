33a899a9001d26421c7c9fdb3468bca1176b01c2
benning55 <60070109@kmitl.ac.th>
2019-04-26 00:23:44 +0700

    add home pages

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'owner': 'Benning',
+        'model': 'Yamaha yzf-r3 2019',
+        'date_post': '25/04/2019',
+        'prices': [{
+            'hour': 50,
+            'day': 100,
+            'week': 1000,
+            'month': 5000
+        }],
+        'pic': 'images/r3.jpg'
+    },
+    {
+        'owner': 'Muse',
+        'model': 'Yamaha yzf-r15 2017',
+        'date_post': '25/04/2019',
+        'prices': [{
+            'hour': 50,
+            'day': 100,
+            'week': 1000,
+            'month': 5000
+        }],
+        'pic': 'images/r15.jpg'
+    }
+]
+
 
 def home(request):
-    return render(request, 'posts/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/home.html', context=context)
+
+
+def about(request):
+    return render(request, 'posts/about.html')
