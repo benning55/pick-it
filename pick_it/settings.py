88f4e7b67ecf433893b33ec71fba9e99c951043e
benning55 <60070109@kmitl.ac.th>
2019-05-09 11:39:21 +0700

    `add

--- pick_it/settings.py
+++ pick_it/settings.py
@@ -143,7 +143,7 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'accounts/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'accounts/static')
 ]
