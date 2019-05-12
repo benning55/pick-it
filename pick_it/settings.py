79ec34c807c1cb8c48a3a87a41c4403f33496d0c
benning55 <60070109@kmitl.ac.th>
2019-05-12 12:47:59 +0700

    pic:1

--- pick_it/settings.py
+++ pick_it/settings.py
@@ -144,9 +144,6 @@ EMAIL_PORT = 587
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'accounts/static')
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
